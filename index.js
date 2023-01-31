dd813fd4bc06823f2bbf6a29eb457ccd8ea0f871
dead-alchemy <dlonicholas@gmail.com>
2023-01-30 22:23:21 -0700

    adding log.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var cron = require("node-cron");
 var tweet = require("./functions/tweet");
 
+console.log("Starting....");
 cron.schedule("06 14 * * 0", () => {
 	tweet.isopod_tweet();
 });
