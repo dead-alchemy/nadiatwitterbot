4ea463d407ed4e037a8a87880561876bd6687625
dead-alchemy <dlonicholas@gmail.com>
2023-02-13 22:34:14 -0700

    testing.

---
+++ functions/test.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const {schedule} = require("@netlify/functions");
+
+const {twitterClient} = require("../client");
+//const {canned_tweets} = require("../consts");
+
+const test_tweet = async () => {
+	try {
+		await twitterClient.v2.tweet("Test Tweet");
+		console.log("tweeted: " + "Test Tweet");
+	} catch (e) {
+		console.log(e);
+	}
+	return;
+};
+
+exports.handler = schedule("36 5 * * *", test_tweet);
